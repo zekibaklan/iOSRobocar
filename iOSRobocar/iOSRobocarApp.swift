97ed90a989367e669a9a3e5dc51c390740ebdcd1
zekibaklan <zekibaklan@icloud.com>
2024-03-12 18:45:14 +0300

    Initial Commit

---
+++ iOSRobocar/iOSRobocarApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSRobocarApp.swift
+//  iOSRobocar
+//
+//  Created by Zeki Baklan on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct iOSRobocarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
